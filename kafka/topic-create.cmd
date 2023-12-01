f045c84281e4f87cdbd351e918be60f36d800a1c
osman yaycioglu <osman.yaycioglu@gmail.com>
2023-12-01 09:22:41 +0300

    day 2 1

--- kafka/topic-create.cmd
+++ kafka/topic-create.cmd
@@ -1,1 +1,1 @@
-kafka-topics.bat --create --zookeeper localhost:2181 --topic invoice --partitions 5 --replication-factor 3 --config segment.bytes=1000000
+kafka-topics.bat --create --bootstrap-server 127.0.0.1:9092 --topic first-topic --partitions 5 --replication-factor 3 --config segment.bytes=1000000
